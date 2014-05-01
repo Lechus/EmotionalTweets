e06c55a10b454788517183e42774886b9792fa94
Lechus <lechus@master.pl>
2014-05-01 16:57:56 +0200

    Can receive tweets by term or hash tag

--- app/routes.php
+++ app/routes.php
@@ -10,13 +10,34 @@
 | and give it the Closure to execute when that URI is requested.
 |
 */
-
 Route::get('/', function()
 {
-    //Returns a collection of the most recent Tweets posted by the user indicated by the screen_name or user_id parameters.
-    $search = Input::get('q'); 
-    $tweets = Twitter::getUserTimeline(array('screen_name' => $search, 'count' => 5));
-    return View::make('tweets', array('tweets' => $tweets));
+    //Returns a collection of the most recent Tweets by search terms or hash tags
+    $search = '';
+    $tweets = null;
+    return View::make('searchtweets', array('tweets' => $tweets, 'q' => $search));
 });
 
+Route::post('/', function()
+{
+    //Returns a collection of the most recent Tweets by search terms or hash tags
+    $search = Input::get('q'); 
+    $tweets =Twitter::getSearch(array('q' => $search, 'count' => 5, 'result_type'=>'recent'));
+    return View::make('searchtweets', array('tweets' => $tweets, 'q' => $search));
+});
 
+Route::get('/screen_name', function()
+{
+    
+    $search = ''; 
+    $tweets = null;
+    return View::make('tweets', array('tweets' => $tweets, 'screen_name' => $search));
+});
+
+Route::post('/screen_name', function()
+{
+    //Returns a collection of the most recent Tweets posted by the user indicated by the screen_name or user_id parameters.
+    $search = Input::get('screen_name'); 
+    $tweets = Twitter::getUserTimeline(array('screen_name' => $search, 'count' => 5));
+    return View::make('tweets', array('tweets' => $tweets, 'screen_name' => $search));
+});
--- app/views/searchtweets.blade.php
+++ app/views/searchtweets.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
     <head>
         <meta charset="UTF-8">
-        <title>Emotionals Tweets :: Search by Screen_name</title>
+        <title>Emotionals Tweets</title>
 {{ HTML::style('tb/css/bootstrap.min.css') }}
 {{ HTML::style('tb/css/bootstrap-theme.min.css') }}
 {{ HTML::script('tb/js/bootstrap.min.js') }}
@@ -11,10 +11,10 @@
         <h1>Search for tweets.</h1>
         
 <div class="row">
-	{{ Form::open(array('url'=>'/screen_name', 'class'=>'form-search', 'role'=>'form', 'method' => 'post')) }}
+	{{ Form::open(array('url'=>'/', 'class'=>'form-search', 'role'=>'form', 'method' => 'post')) }}
 	<div class="col-lg-6">
 		<div class="input-group">
-			{{ Form::text('screen_name', $screen_name, array('class'=>'form-control', 'placeholder'=>'Search by screen_name only.'))}}
+			{{ Form::text('q', $q, array('class'=>'form-control', 'placeholder'=>'Search term or hash tag.'))}}
 			<span class="input-group-btn">
 				{{ Form::submit('Search', array('class'=>'btn btn-default'))}}
 			</span>
@@ -22,20 +22,26 @@
 	</div><!-- /.col-lg-6 -->
 	{{ Form::close() }}
 </div><!-- /.row -->
-<p>Looking for searching by  <a href="{{URL::to('/')}}">term or hast tags?</a></p>
+<p>Looking for searching by  <a href="{{URL::to('/screen_name')}}">screen_name?</a></p>
         <br/>
-
+        
+        <?php
+        //var_dump($tweets);
+        ?>
         <div class="twitter">
-            @if(!empty($tweets))
-                @foreach($tweets as $tweet)
+            @if(!empty($tweets))       
+                  
+            @foreach($tweets->statuses as $tweet)
+
                     <div class="twitter-block">
                         {{$tweet->user->name}} ({{Twitter::linkify('@'.$tweet->user->screen_name)}})
                         <p>{{Twitter::linkify($tweet->text)}}</p>
                         <span class="timeago" title="{{$tweet->created_at}}">{{ date('H:i, M d', strtotime($tweet->created_at)) }}</span>
                     </div>
-                @endforeach
+ 
+            @endforeach
             @else
-               @if(Request::isMethod('post'))
+                @if(Request::isMethod('post'))
                     <p>We are having a problem with our Twitter Feed right now.</p>
                 @endif
             @endif

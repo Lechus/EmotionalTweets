12d925fd522c7f1040b9dbd9f6c6795159f00469
Lechus <lechus@master.pl>
2014-05-08 04:38:59 +0200

    UnirestAnalysis tests

---
+++ app/tests/Lpp/UnirestAnalysisTest.php
@@ -0,0 +1,55 @@
+<?php
+
+use Lpp\Analysis\UnirestAnalysis;
+
+class UnirestAnalysisTest extends TestCase
+{
+
+    /**
+     * Tears down the fixture, for example, closes a network connection.
+     * This method is called after a test is executed.
+     */
+    protected function tearDown()
+    {
+        Mockery::close();
+    }
+
+    /**
+     * @covers Lpp\Analysis\UnirestAnalysis::analyse
+     */
+    public function testAnalyse()
+    {
+        
+        $unirestMock = Mockery::mock('Unirest');
+        $unirestResponse = $unirestMock
+                ->shouldReceive('post')
+                ->once()
+                ->andReturn($this->getMashapeUnirestResponse());
+        
+        $unirestAnalyser = new UnirestAnalysis($unirestMock);
+        
+        //Act
+        $emotion = $unirestAnalyser->analyse('text', 'en');
+
+        //Assert
+        $this->assertEquals('Happy', $emotion, 'It should be Happy'); 
+    }
+    
+   /**
+    * Return custom Unirest response object
+    * @return object
+    */
+    protected function getMashapeUnirestResponse()
+    {        
+        $response = new stdClass();
+        $body = new stdClass();
+        $body->language = "en";
+        $body->value = 0.50055610583463;
+        $body->sent  = 1;
+        
+        $response->body = $body;
+    
+        return $response;   
+    }  
+    
+}
